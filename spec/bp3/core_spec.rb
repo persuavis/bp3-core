17278d5b531f43867c5d85808bfa97012e333c73
Wim den Braven <wimdenbraven@persuavis.com>
2023-12-19 14:25:23 -0800

    Initial commit

---
+++ spec/bp3/core_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bp3::Core do
+  it "has a version number" do
+    expect(Bp3::Core::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
